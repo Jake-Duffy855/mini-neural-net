bd2a778b022bf63231a180f5a59faff4339a9d5c
Jake Duffy <duffyjemmett@gmail.com>
2019-12-15 21:01:11 -0600

    Added testing file.

---
+++ net_testing.py
@@ -0,0 +1,36 @@
+import net01 as n
+import random
+import math
+
+lr = 0.1
+a = n.Net(3, [2, 4, 1])
+# a.set_activation(lambda x: math.log10(abs(x)+0.1))
+
+for i in range(30000):
+    random.choice([a.train([0, 0], [1], lr),
+                   a.train([0, 1], [0], lr),
+                   a.train([1, 0], [0], lr),
+                   a.train([1, 1], [1], lr)])
+
+print(a.get_out([0, 0]))
+print(a.get_out([0, 1]))
+print(a.get_out([1, 0]))
+print(a.get_out([1, 1]))
+print()
+print(a)
+
+# b = n.Net(3, [2, 4, 1])
+#
+# print(b.get_out([0, 0]))
+# print(b.get_out([0, 1]))
+# print(b.get_out([1, 0]))
+# print(b.get_out([1, 1]))
+# print()
+#
+# b = n.make_net(3, [2, 4, 1], a.cons, a.biases)
+#
+# print(b.get_out([0, 0]))
+# print(b.get_out([0, 1]))
+# print(b.get_out([1, 0]))
+# print(b.get_out([1, 1]))
+# print()
