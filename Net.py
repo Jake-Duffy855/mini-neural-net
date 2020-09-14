dd934da987cd82c4d9825a94157f09b79f8512e8
Jake Duffy <duffyjemmett@gmail.com>
2020-09-13 22:38:55 -0400

    Completed switch to np arrays and matrix algebra

--- Net.py
+++ Net.py
@@ -26,6 +26,7 @@ def relu(x):
 
 # calculates gradient of a function at a specific value, used for descent
 def gradient(func, x, delta=0.001):
+    vectorized_func = np.vectorize(func)
     return (func(x + delta) - func(x)) / delta
 
 
@@ -46,8 +47,8 @@ class Net:
         self.biases = []
         self.activate = act
         for layer in range(self.layers):
-            self.cons.append(np.random.rand(nodes[layer], nodes[layer + 1]))
-            self.biases.append(np.random.rand(nodes[layer + 1], 1))
+            self.cons.append(np.random.uniform(1, 1, (nodes[layer + 1], nodes[layer])))
+            self.biases.append(np.random.uniform(1, 1, (nodes[layer + 1], 1)))
 
     # change the activation function
     def set_activation(self, fun):
@@ -55,70 +56,40 @@ class Net:
 
     # returns a string containing formatted weights and biases arrays
     def __str__(self):
-        result = "["
-        for i in range(len(self.cons)):
-            result += "["
-            for j in range(len(self.cons[i])):
-                result += str(self.cons[i][j])
-                if j != len(self.cons[i]) - 1:
-                    result += ",\n"
-            result += "]"
-            if i != len(self.cons) - 1:
-                result += ",\n"
-        result += "]"
-
-        result += "\n\n["
-        for i in range(len(self.biases)):
-            result += str(self.biases[i])
-            if i != len(self.biases) - 1:
-                result += ",\n"
-        result += "]"
-
-        return result
+        return str(self.cons) + "\n\n" + str(self.biases)
 
     # feeds forward an input np array and returns the calculated output
     def get_out(self, inp):
+        vectorized_activation = np.vectorize(self.activate)
         for layer in range(self.layers):
-            inp = self.activate(inp)
+            inp = vectorized_activation(inp)
             inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
-        return self.activate(inp)
+        return vectorized_activation(inp)
 
     # trains the net on a given input array, desired output array, and learning rate (default 0.1)
     def train(self, inp, exp, lr=0.1):
+        vectorized_activation = np.vectorize(self.activate)
         inputs = [inp]
         for layer in range(self.layers):
-            inp = list(map(self.activate, inp))
-            nxt = []
-            for i in range(self.nodes[layer + 1]):
-                s = self.biases[layer][i]
-                for j in range(self.nodes[layer]):
-                    s += self.cons[layer][i][j] * inp[j]
-                nxt.append(s)
-            inp = nxt
-            inputs.append(nxt)
+            inp = vectorized_activation(inp)
+            inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
+            inputs.append(inp)
 
         # inputs has all node values from inp to out (not activated)
 
         # has been activated
-        out = list(map(self.activate, nxt))
+        out = vectorized_activation(inp)
 
         # errors will have all errors from out to first hidden in that order (activated)
-        errors = [[(exp[i] - out[i]) for i in range(max(len(out), len(exp)))]]
+        errors = [exp - out]
 
         for layer in range(self.layers - 1):
-            err = []
-            for i in range(self.nodes[-(layer + 2)]):
-                s = 0
-                for j in range(self.nodes[-(layer + 1)]):
-                    s += self.cons[-(layer + 1)][j][i] * errors[layer][j]
-                err.append(s)
-            errors.append(err)
+            errors.append(np.matmul(np.transpose(self.cons[-(layer + 1)]), errors[layer]))
 
         for layer in range(self.layers):
-            for i in range(self.nodes[-(layer + 1)]):
-                for j in range(self.nodes[-(layer + 2)]):
-                    self.cons[-(layer + 1)][i][j] += \
-                        lr * errors[layer][i] * gradient(self.activate, inputs[-(layer + 1)][i])\
-                        * self.activate(inputs[-(layer + 2)][j])
-                self.biases[-(layer + 1)][i] += \
-                    lr * errors[layer][i] * gradient(self.activate, inputs[-(layer + 1)][i])
+            # adjust connection weights
+            self.cons[-(layer + 1)] += np.matmul(
+                lr * errors[layer] * gradient(vectorized_activation, inputs[-(layer + 1)]),
+                np.transpose(inputs[-(layer + 2)]))
+            # adjust bias weights
+            self.biases[-(layer + 1)] += lr * errors[layer] * gradient(vectorized_activation, inputs[-(layer + 1)])
