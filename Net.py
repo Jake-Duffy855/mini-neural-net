2b0a459a026bd5c2097c5f3cec408a32334023f7
Jake Duffy <duffyjemmett@gmail.com>
2021-02-23 21:04:29 -0500

    first branch cmmmit

--- Net.py
+++ Net.py
@@ -15,28 +15,18 @@ def sigmoid(x):
 
 
 # default activation function
-def tanh(v):
-    w = np.copy(v)
-    for i in range(w.size):
-        w[(i, 0)] = 1 - sigmoid(-2 * w[(i, 0)])
-    return w
+def tanh(x):
+    return 1 - sigmoid(-2 * x)
 
 
 # relu
 def relu(x):
     return max(0, x)
-relu = np.vectorize(relu)
-
-
-def softmax(v):
-    w = np.copy(v)
-    for i in range(w.size):
-        w[(i, 0)] = math.e ** w[(i, 0)]
-    return w / sum(w)
 
 
 # calculates gradient of a function at a specific value, used for descent
 def gradient(func, x, delta=0.001):
+    vectorized_func = np.vectorize(func)
     return (func(x + delta) - func(x)) / delta
 
 
@@ -48,10 +38,10 @@ class Net:
         self.biases = []
         self.activate = act
 
-        if self.activate == []:
+        if len(self.activate) == 0:
             self.activate = [tanh for i in range(self.layers + 1)]
             self.activate[0] = tanh
-            self.activate[-1] = softmax
+            self.activate[-1] = tanh
 
         for layer in range(self.layers):
             self.cons.append(np.random.uniform(-1, 1, (nodes[layer + 1], nodes[layer])))
@@ -68,22 +58,22 @@ class Net:
     # feeds forward an input np array and returns the calculated output
     def get_out(self, inp):
         for layer in range(self.layers):
-            inp = self.activate[layer](inp)
+            inp = np.vectorize(self.activate[layer])(inp)
             inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
-        return self.activate[-1](inp)
+        return np.vectorize(self.activate[-1])(inp)
 
     # trains the net on a given input array, desired output array, and learning rate (default 0.1)
     def train(self, inp, exp, lr=0.1):
         inputs = [inp]
         for layer in range(self.layers):
-            inp = self.activate[layer](inp)
+            inp = np.vectorize(self.activate[layer])(inp)
             inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
             inputs.append(inp)
 
         # inputs has all node values from inp to out (not activated)
 
         # has been activated
-        out = self.activate[-1](inp)
+        out = np.vectorize(self.activate[-1])(inp)
 
         # errors will have all errors from out to first hidden in that order (activated)
         errors = [exp - out]
@@ -94,8 +84,113 @@ class Net:
         for layer in range(self.layers):
             # adjust connection weights
             self.cons[-(layer + 1)] += np.matmul(
-                lr * errors[layer] * gradient(self.activate[-(layer + 1)], inputs[-(layer + 1)]),
+                lr * errors[layer] * gradient(np.vectorize(self.activate[-(layer + 1)]), inputs[-(layer + 1)]),
                 np.transpose(inputs[-(layer + 2)]))
             # adjust bias weights
             self.biases[-(layer + 1)] += lr * errors[layer] * gradient(
-                self.activate[-(layer + 1)], inputs[-(layer + 1)])
+                np.vectorize(self.activate[-(layer + 1)]), inputs[-(layer + 1)])
+
+# import random
+# import math
+# import numpy as np
+# import matplotlib.pyplot as pp
+#
+#
+# # default function to initialize weights and biases
+# def rand():
+#     return random.uniform(-1, 1)
+#
+#
+# # basic activation function
+# def sigmoid(x: int):
+#     try:
+#         return 1 / (1 + math.exp(-x))
+#     except OverflowError:
+#         return 0
+#
+#
+# # default activation function
+# def tanh(v: np.ndarray):
+#     w = np.copy(v)
+#     for i in range(w.size):
+#         w[(i, 0)] = 1 - sigmoid(-2 * w[(i, 0)])
+#     return w
+#
+#
+# # relu
+# def relu(x: int):
+#     return max(0, x)
+# relu = np.vectorize(relu)
+#
+#
+# def softmax(v: np.ndarray):
+#     w = np.copy(v)
+#     for i in range(w.size):
+#         w[(i, 0)] = math.e ** w[(i, 0)]
+#     return w / sum(w)
+#
+#
+# # calculates gradient of a function at a specific value, used for descent
+# def gradient(func, x, delta=0.001):
+#     return (func(x + delta) - func(x)) / delta
+#
+#
+# class Net:
+#     def __init__(self, layers, nodes, act=[], r=rand):
+#         self.layers = layers - 1
+#         self.nodes = nodes
+#         self.cons = []
+#         self.biases = []
+#         self.activate = act
+#
+#         if self.activate == []:
+#             self.activate = [tanh for i in range(self.layers + 1)]
+#             self.activate[0] = tanh
+#             self.activate[-1] = softmax
+#
+#         for layer in range(self.layers):
+#             self.cons.append(np.random.uniform(-1, 1, (nodes[layer + 1], nodes[layer])))
+#             self.biases.append(np.random.uniform(-1, 1, (nodes[layer + 1], 1)))
+#
+#     # change the activation function
+#     def set_activation(self, act):
+#         self.activate = act
+#
+#     # returns a string containing formatted weights and biases arrays
+#     def __str__(self):
+#         return str(self.cons) + "\n\n" + str(self.biases)
+#
+#     # feeds forward an input np array and returns the calculated output
+#     def get_out(self, inp):
+#         for layer in range(self.layers):
+#             inp = self.activate[layer](inp)
+#             inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
+#         return self.activate[-1](inp)
+#
+#     # trains the net on a given input array, desired output array, and learning rate (default 0.1)
+#     def train(self, inp, exp, lr=0.1):
+#         inputs = [inp]
+#         for layer in range(self.layers):
+#             inp = self.activate[layer](inp)
+#             inp = np.matmul(self.cons[layer], inp) + self.biases[layer]
+#             inputs.append(inp)
+#
+#         # inputs has all node values from inp to out (not activated)
+#
+#         # has been activated
+#         out = self.activate[-1](inp)
+#
+#         # errors will have all errors from out to first hidden in that order (activated)
+#         errors = [exp - out]
+#
+#         for layer in range(self.layers - 1):
+#             errors.append(np.matmul(np.transpose(self.cons[-(layer + 1)]), errors[layer]))
+#
+#         for layer in range(self.layers):
+#             # adjust connection weights
+#             self.cons[-(layer + 1)] += np.matmul(
+#                 lr * errors[layer] * gradient(self.activate[-(layer + 1)], inputs[-(layer + 1)]),
+#                 np.transpose(inputs[-(layer + 2)]))
+#             # adjust bias weights
+#             self.biases[-(layer + 1)] += lr * errors[layer] * gradient(
+#                 self.activate[-(layer + 1)], inputs[-(layer + 1)])
--- net_testing.py
+++ net_testing.py
@@ -1,3 +1,4 @@
+
 import Net as n
 import random
 import math
@@ -6,7 +7,8 @@ import matplotlib.pyplot as pp
 
 lr = 0.05
 a = n.Net(4, [2, 4, 3, 1])
-a.set_activation(n.tanh)
+print(a.activate)
+# a.set_activation(n.tanh)
 
 print(a.get_out([[0], [0]]))
 print(a.get_out([[0], [1]]))
@@ -15,11 +17,11 @@ print(a.get_out([[1], [1]]))
 print(a.get_out([[0.75], [0]]))
 print()
 
-for i in range(5000):
-    random.choice([a.train([[0], [0]], [[0]], lr),
-                   a.train([[1], [0]], [[1]], lr),
-                   a.train([[0], [1]], [[1]], lr),
-                   a.train([[1], [1]], [[0]], lr)])
+for i in range(10000):
+    random.choice([a.train(np.array([[0], [0]]), np.array([[0]]), lr),
+                   a.train(np.array([[1], [0]]), np.array([[1]]), lr),
+                   a.train(np.array([[0], [1]]), np.array([[1]]), lr),
+                   a.train(np.array([[1], [1]]), np.array([[0]]), lr)])
     if i % 50 == 0:
         pp.plot(i, a.get_out([[0], [1]]), 'bo')
 
@@ -29,4 +31,3 @@ print(a.get_out([[1], [0]]))
 print(a.get_out([[1], [1]]))
 print(a.get_out([[0.75], [0]]))
 pp.show()
-
